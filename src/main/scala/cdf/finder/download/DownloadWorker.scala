d89e9dcae4b6ddb6d6144a85b438345471ccacbb
Łukasz Jędrzejewski <jedrzejewskiluk@gmail.com>
2016-01-09 12:14:56 +0100

    Specify connection timeouts

--- src/main/scala/cdf/finder/download/DownloadWorker.scala
+++ src/main/scala/cdf/finder/download/DownloadWorker.scala
@@ -1,5 +1,6 @@
 package cdf.finder.download
 
+import java.net.URL
 import java.nio.charset.StandardCharsets
 
 import akka.actor.{Actor, ActorLogging, Props}
@@ -20,11 +21,19 @@ class DownloadWorker extends Actor with ActorLogging {
       // We block inside actor because work is distributed by the parent.
       // See previous revision for a solution with futures.
       Try {
-        Source.fromURL(url, StandardCharsets.ISO_8859_1.name()).mkString
+        downloadURL(url)
       } recover {
         case t =>
           log.error(t, "Downloading from url {} failed", url)
           ""
       } map(Downloader.DownloadResult(id, _)) foreach(sender ! _)
+  }
+
+  private def downloadURL(url: String): String = {
+    val urlObject = new URL(url)
+    val connection = urlObject.openConnection
+    connection.setConnectTimeout(5000)
+    connection.setReadTimeout(3000)
+    Source.fromInputStream(connection.getInputStream, StandardCharsets.ISO_8859_1.name()).mkString
   }
 }
