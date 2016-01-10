214e05ee7c490f8429bf26e35559ad2b85b14e63
Igor Rodzik <igor.rodzik@gmail.com>
2016-01-10 21:19:52 +0100

    Matras shop integration

--- src/main/scala/cdf/finder/matras/MatrasFinderUtil.scala
+++ src/main/scala/cdf/finder/matras/MatrasFinderUtil.scala
@@ -3,6 +3,7 @@ package cdf.finder.matras
 import java.text.NumberFormat
 import java.util.Locale
 
+import cdf.finder.FinderUtil
 import cdf.offer.Offer
 import net.ruippeixotog.scalascraper.browser.Browser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
@@ -11,27 +12,24 @@ import org.jsoup.nodes.Element
 
 import scala.util.Try
 
-class MatrasUtil {
+class MatrasFinderUtil extends FinderUtil {
   private val browser = new Browser
-  """html body div:eq(2) div div:eq(5) div div:eq(0) div:eq(1) div:eq(0) div:eq(0)
- div:eq(4) div"""
-  private val searchResultSelector = """
-      |html body div:eq(4) div div:eq(5) div div:eq(1) div:eq(1) div:eq(2) ol li div div:eq(0) div:eq(0)
-      |div:eq(0) h2 a""".stripMargin
-  private val titleSelector = """
-      |html body div:eq(2) div div:eq(5) div div:eq(0) div:eq(1) div:eq(0) div:eq(0) div:eq(0) div:eq(0) h1""".stripMargin
 
-  private val priceSelector = """html body div:eq(2) div div:eq(5) div div:eq(0) div:eq(1) div:eq(0) div:eq(0) div:eq(1)
-     | form div:eq(1) div p:eq(0) span:eq(1)""".stripMargin
+  private val searchResultSelector = "h2.product-name a"
+  private val titleSelector = "div.product-name h1"
+
+  private val priceSelector = "p.special-price span.price"
   
-  private val authorSelector =
-    """ html body div:eq(3) div div:eq(6) div div:eq(1) div:eq(2) div:eq(1) div:eq(1)
-      | div:eq(1) div:eq(2) h3:eq(1) a""".stripMargin
+  private val authorSelector = "div.product-info-list h3:eq(0) a"
+
+  private val descriptionSelector = "div.short-description div"
 
   def createSearchQuery(query: String): String = {
     val escapedQuery = query.replace(' ', '+')
     "http://matras.pl/szukaj?query=" + escapedQuery + "&kategoria=65"
   }
+
+  def shopName: String = "Matras"
 
   def parseSearchResults(source: String): List[String] = {
     val document = browser.parseString(source)
@@ -48,11 +46,13 @@ class MatrasUtil {
       val price = NumberFormat.getInstance(Locale.forLanguageTag("PL"))
         .parse(document >> text(priceSelector))
         .toString
+      val description = document >> text(descriptionSelector)
       Offer(
         title = title,
         url = url,
         price = BigDecimal(price),
-        author = author
+        author = author,
+        description = description
       )
     }
   }
